0cc750fd7a6fb41dc197077f830945ae265a62dd
Signus0001 <103461996+DOG-ControlOfficial@users.noreply.github.com>
2023-02-12 11:38:59 +0100

    Create BotControl.lua

---
+++ BotControl.lua
@@ -0,0 +1,13 @@
+repeat wait() until game:IsLoaded() == true
+wait(8)
+
+local players = game:GetService("Players")
+local localplayer = players.LocalPlayer
+repeat wait() until localplayer.Character and localplayer.Character.HumanoidRootPart
+local HumanoidRootPart = localplayer.Character:WaitForChild("HumanoidRootPart")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TeleportService = game:GetService("TeleportService")
+local VirtualUser = game:GetService("VirtualUser")
+local StarterGui = game:GetService("StarterGui")
+
+-- so I decided to not leak the full bot source, (Prison Life) it should be here now but I wont leak this due to it all being broken (like legit 90% of it is broken) so yea bye nuggies
